f02fb1bf9fd3e98cc3ed07b527c18720a044b482
Him0824 <126760063+Him0824@users.noreply.github.com>
2023-12-09 20:40:03 +0800

    Update CreateCampaign.jsx

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -51,7 +51,7 @@ const CreateCampaign = () => {
         <div className="flex flex-wrap gap-[40px]">
           <FormField 
             labelName="Your Name *"
-            placeholder="John Doe"
+            placeholder="Isaac LO"
             inputType="text"
             value={form.name}
             handleChange={(e) => handleFormFieldChange('name', e)}
